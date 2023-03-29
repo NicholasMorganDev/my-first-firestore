28e5e56a54e3c528188ec4f07144f216830c62fe
Nicholas Morgan <Nicholas.Morgan2002@aol.com>
2023-03-29 15:50:00 -0400

    finished up firestore intro

--- index.js
+++ index.js
@@ -14,15 +14,42 @@ initializeApp({
 const db = getFirestore();
 
 //add a product to our products collection
-const candy = {
-  name: 'Skittles',
-  unitPrice: 3.99,
-  size: '16 oz',
-  color: 'green',
-  inventory: 144,
-  productNumber: 7,
+const candy2 = {
+  name: 'Twix',
+  unitPrice: 2.99,
+  size: '12 oz',
+  color: 'gold',
+  inventory: 288,
+  productNumber: 2,
 }
 
-db.collection('products').add(candy)
-  .then(doc => console.log('added doc: ' + doc.id))
-  .catch(err => console.log(err))
+//add document to firestore
+// db.collection('products').add(candy2)
+//   .then((doc) => {
+//   console.log('added doc: ' + doc.id)
+// })
+// .catch(err => console.log(err))
+
+//how to read a document from firestore
+//db.collection('products').doc('FNFqop0KgKpocmY8UywG').delete()
+db.collection('products').doc('FNFqop0KgKpocmY8UywG').get()
+.then((doc) => {
+  //console.log(doc.data())
+})
+.catch(console.log)
+
+//how to update a document in firestore
+db.collection('products').doc('FNFqop0KgKpocmY8UywG').update({
+  inventory: 555,
+  //will add if their isn't already a key value pair
+  customerFavorite: true
+})
+
+//how to get a whole collection
+db.collection('products').get()
+.then(collection => {
+  const productList = collection.docs.map(doc => ({...doc.data(), id: doc.id}))
+  console.table(productList)
+})
+.catch(console.log)
+
